5b460241d081c4c93165f66f7cb97caf6129a61c
sara <sara@oak-fern.com>
2023-06-03 17:49:21 -0700

    Add grid resize button and function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,38 @@
+const button = document.createElement('button');
+button.textContent = "Set grid size";
+
+
+const body = document.querySelector('body');
 const container = document.querySelector('#container');
 
-const gridSize = 16;
+body.prepend(button);
 
-for (let x = 0; x < gridSize; x++) {
-    const row = document.createElement('div');
-    row.className = "row";
-    container.appendChild(row);
+gridPainter(16)
 
-    for (let y = 0; y < gridSize; y++) { 
-        const box = document.createElement('div');
-        box.className = "box";
-        box.addEventListener('mouseover', e => {
-            box.style.backgroundColor = 'red';
-        })
+button.addEventListener('click', e => {
+    console.log(e)
+    gridPainter(prompt('Enter a number to set a new grid size'))
+})
 
-        box.addEventListener('mouseout', () => {
-            box.style.backgroundColor = 'white';
-        })
-        row.appendChild(box);
+function gridPainter(gridSize) {
+    container.innerHTML = "";
+    for (let x = 0; x < gridSize; x++) {
+        const row = document.createElement('div');
+        row.className = "row";
+        container.appendChild(row);
+    
+        for (let y = 0; y < gridSize; y++) { 
+            const box = document.createElement('div');
+            box.className = "box";
+            box.addEventListener('mouseover', e => {
+                box.style.backgroundColor = 'red';
+            })
+    
+            box.addEventListener('mouseout', () => {
+                box.style.backgroundColor = 'white';
+            })
+            row.appendChild(box);
+        }
     }
 }
+

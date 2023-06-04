4375880e02d44fdb4b73f1119ad2e5af76d47095
sara <sara@oak-fern.com>
2023-06-03 17:20:57 -0700

    Add feature to color/uncolor boxes on hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -10,6 +10,13 @@ for (let x = 0; x < gridSize; x++) {
     for (let y = 0; y < gridSize; y++) { 
         const box = document.createElement('div');
         box.className = "box";
+        box.addEventListener('mouseover', e => {
+            box.style.backgroundColor = 'red';
+        })
+
+        box.addEventListener('mouseout', () => {
+            box.style.backgroundColor = 'white';
+        })
         row.appendChild(box);
     }
 }
